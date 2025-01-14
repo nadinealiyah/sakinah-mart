fa8575f01872ef93cd151548d1e6243bb63637f2
Nadine <nadinealiyah123@gmail.com>
2025-01-14 17:50:22 +0700

    resize the font

--- helper/custom_metric_card.py
+++ helper/custom_metric_card.py
@@ -4,10 +4,10 @@ def metric_card(title, icon, content, description, color, text_color="white"):
     st.markdown(
         f"""
         <div style="background-color: {color}; padding: 10px 20px; border-radius: 20px">
-            <h6 style="margin: 0px; font-size:13px; color:{text_color}">{title}</h6>
+            <h6 style="margin: 0px; font-size:17px; color:{text_color}">{title}</h6>
             <h2 style="margin: -25px 0px -15px; font-size:25px; color:{text_color}">
                 <i class="{icon}" style="margin-right: 10px;"></i>{content}</h2>
-            <p style="margin: 0px; font-size:12px; color:{text_color}">{description}</p>
+            <p style="margin: 0px; font-size:15px; color:{text_color}">{description}</p>
         </div>
         """,
         unsafe_allow_html=True
